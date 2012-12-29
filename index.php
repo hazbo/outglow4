a58f4c880a560a30b5cab4fba7d5d8a2f3bdf079
Dev <Dev@MacBook-Pro.local>
2012-12-29 00:58:48 +0000

    Removed unneeded files ect..

--- index.php
+++ index.php
@@ -6,32 +6,15 @@
 	require_once('Outglow/Bootstrap.php');
 	require_once('Outglow/Configure.php');
 	require_once('Outglow/Autorouter.php');
-	/*
-	require_once('Outglow/Tools.php');
-	require_once('Outglow/Bootstrap.php');
-	require_once('Outglow/Cache.php');
+
 	if (file_exists('vendor/autoload.php')) {
 		require_once('vendor/autoload.php');
-	}*/
-
-	$bootstrap = new Bootstrap(new Configure());
-
-
+	}
 
 	/**
 	 * CREATE THE BOOTSTRAP
 	 */
-	#$bootstrap = new Bootstrap();
-
-	/**
-	 * SEE IF WE HAVE ANY CACHING TO DO
-	 * IF YOU'RE READING THIS, AND YOU
-	 * DO NOT HAVE MEMCACHE INSTALLED
-	 * AND RUNNING, GO GET IT! DON'T
-	 * GET ME WRONG, YOU DON'T NEED IT
-	 * TO RUN OUTGLOW, BUT IT'S WORTH IT
-	 */
-	#$cache = new Cache($bootstrap);
+	$bootstrap = new Bootstrap(new Configure());
 
 	/**
 	 * FIRE THE BOOTSTRAP AND
